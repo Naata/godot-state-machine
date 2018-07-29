ed6def7b2e073d672d09f854fe924ddcd490f732
Marcin Kuna <kuna.marcin@gmail.com>
2018-07-29 17:29:05 +0200

    add abstract base state

---
+++ base_state.gd
@@ -0,0 +1,18 @@
+extends Node
+
+signal state_change(next_state, only_state)
+
+func state_update(delta):
+    pass
+
+func state_handle_input(event):
+    pass
+
+func state_enter():
+    pass
+
+func state_exit():
+    pass
+
+func state_name():
+    return name
